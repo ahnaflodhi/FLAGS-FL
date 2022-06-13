cd172e0fdb569efd60fcae0958a54826560cc500
Ahnaf Lodhi <alodhi18@ku.edu.tr>
2022-06-13 12:03:59 +0300

    Removed Arguments-Use get-args for arguments henceforth

--- Main-Fed.py
+++ Main-Fed.py
@@ -7,6 +7,7 @@ import sys, argparse
 import pickle
 import time
 import gc
+from get_args import arg_parser
 from utils import dataset_approve, save_file, model_size
 from data_utils import * # Returns the train and test sets for the chosen dataset; dataset_select and class DataSubset
 from data_dist import * # (Returns the dictionary of nodes/data partitions for both iid and nidd) )
@@ -15,22 +16,7 @@ from env_sysmodel import system_model, FL_Modes
 from devices import Nodes, Servers
 from plots import plot_testacc
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-b', type = int, default = 8, help = 'Batch size for the dataset')
-parser.add_argument('-t', type = int, default = 8, help = 'Batch size for the test dataset')
-parser.add_argument('-d', type = str, default = 'mnist', help='Name of the dataset : mnist, cifar10 or fashion')
-parser.add_argument('-n', type = int, default = 40, help='Number of nodes')
-parser.add_argument('-c', type = int, default = 7, help='Number of clusters')
-parser.add_argument('-ser', type = int, default = 3, help='Number of servers')
-parser.add_argument('-e', type = int, default = 1, help='Number of epochs')
-parser.add_argument('-r', type = int, default = 30, help='Number of federation rounds')
-parser.add_argument('-o', type = float, default = 0.75, help='Overlap factor in cluser boundaries')
-parser.add_argument('-s', type = int, default = 50, help = ' Shard size for Non-IID distribution')
-parser.add_argument('-prop', type = float, default = 1.0, help = 'Proportion of nodes chosen for server aggregation : 0.0-1.0')
-parser.add_argument('-aggprop', type = float, default = 1.0, help = 'Aggregation-Proportion: Proportion of nodes in neighborhood for D2D aggregation : 0.0-1.0')
-parser.add_argument('-dist', type = str, default = 'niid', help = 'Data distribution mode (IID, non-IID, 1-class and 2-class non-IID: iid, niid, niid1 or niid2.')
-args = parser.parse_args()
-
+args = arg_parser()
 dataset = args.d
 batch_size = args.b
 nodes = args.n
